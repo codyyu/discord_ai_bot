69797ad0b2eb723260f112a24209d71e8ecdb406
Cody Yu <codyyu717@gmail.com>
2023-10-07 03:08:08 -0700

    adding kafka producer

--- src/discord_ai_bot/cogs/msgDataUpdate.py
+++ src/discord_ai_bot/cogs/msgDataUpdate.py
@@ -1,5 +1,10 @@
+import json
 import logging
+from typing import Union
 
+from confluent_kafka import KafkaError
+from confluent_kafka import Message as KF_Message
+from confluent_kafka import Producer
 from discord import Message
 from discord.ext import commands
 from discord.ext.commands import Bot
@@ -9,6 +14,18 @@ from discord_ai_bot.config import Config
 config = Config()
 logger = logging.getLogger(__name__)
 
+# instantiate kafka producer
+producer = Producer(
+    {"bootstrap.servers": "broker:29092", "client.id": "Discord-Message"}
+)
+
+
+def msg_callback(err: Union[KafkaError, None], msg: KF_Message) -> None:
+    if err:
+        logger.error(f"Failed to deliver msg via kafka: {err}")
+    else:
+        logger.info(f"Produced event to topic: {msg.topic()}")
+
 
 class MsgDataUpdate(commands.Cog):
     def __init__(self, bot: Bot):
@@ -16,7 +33,17 @@ class MsgDataUpdate(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: Message) -> None:
-        pass
+        topic = "messages"
+        msg = {
+            "message_id": message.id,
+            "message_content": message.content,
+            "member_id": message.author.id,
+        }
+        producer.produce(
+            topic=topic, key="message", value=json.dumps(msg), callback=msg_callback
+        )
+        producer.poll(1)
+        producer.flush()
 
 
 async def setup(bot: Bot) -> None:
